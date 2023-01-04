0bf5d6135288856ab554b8643d34a78065cf410b
Võ Thanh Tùng <parkhoon1609@gmail.com>
2023-01-05 00:21:32 +0700

    add comment

--- check_attendance.py
+++ check_attendance.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
-import time
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
-import mysql.connector
-import Adafruit_CharLCD as LCD
+import time    #setup time hệ thống
+import RPi.GPIO as GPIO    #thư viện sử dụng GPIO
+from mfrc522 import SimpleMFRC522   #thư viện sử dụng module RFID
+import mysql.connector    #thư viện sử dụng mysql
+import Adafruit_CharLCD as LCD    #thư viện sử dụng LCD
 
-db = mysql.connector.connect(
+db = mysql.connector.connect(      #tạo database
   host="localhost",
   user="attendanceadmin",
   passwd="raspberry",
   database="attendancesystem"
 )
 
-cursor = db.cursor()
-reader = SimpleMFRC522()
+cursor = db.cursor()   #khởi tạo database
+reader = SimpleMFRC522()    #khởi tạo RFID
 
-lcd = LCD.Adafruit_CharLCD(4, 24, 23, 17, 18, 22, 16, 2, 4);
+lcd = LCD.Adafruit_CharLCD(4, 24, 23, 17, 18, 22, 16, 2, 4);     #khai báo chân LCD
 
-try:
+try:      #thực hiện đọc thẻ card
   while True:
     lcd.clear()
     lcd.message('Place Card to\nrecord attendance')
     id, text = reader.read()
 
-    cursor.execute("Select id, name FROM users WHERE rfid_uid="+str(id))
+    cursor.execute("Select id, name FROM users WHERE rfid_uid="+str(id))   #lưu trữ dữ liệu id
     result = cursor.fetchone()
 
     lcd.clear()
 
     if cursor.rowcount >= 1:
       lcd.message("Welcome " + result[1])
-      cursor.execute("INSERT INTO attendance (user_id) VALUES (%s)", (result[0],) )
+      cursor.execute("INSERT INTO attendance (user_id) VALUES (%s)", (result[0],) )   #chèn user_id vào từng giá trị cột
       db.commit()
     else:
       lcd.message("User does not exist.")
